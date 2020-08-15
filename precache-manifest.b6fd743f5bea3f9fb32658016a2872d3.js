3b17bf4342c8ae92488210c87058c6d1c9412a5b
edward <hughes5683@gmail.com>
2020-08-15 03:45:04 -0500

    Updates

---
+++ precache-manifest.b6fd743f5bea3f9fb32658016a2872d3.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7cc077819e70f6e818d41a4896a1d182",
+    "url": "/index.html"
+  },
+  {
+    "revision": "8a19a22032fa59e89f18",
+    "url": "/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "bb2da9f83535588c12f9",
+    "url": "/static/js/2.cc5c8262.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.cc5c8262.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8a19a22032fa59e89f18",
+    "url": "/static/js/main.d26b5fbd.chunk.js"
+  },
+  {
+    "revision": "a2f64f6813266256d60a",
+    "url": "/static/js/runtime-main.60afb790.js"
+  }
+]);
