f0b1ebfc8f77dbf40abc8c393a9e889cb0aff68a
reddtoric <14900721+reddtoric@users.noreply.github.com>
2026-01-19 00:43:24 -0600

    setup configs + make simple minimal page

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,9 +1,8 @@
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.html',
-  styleUrl: './app.scss'
+	selector: 'app-root',
+	templateUrl: './app.html',
+	styleUrl: './app.scss',
 })
-export class App {
-}
+export class App {}
